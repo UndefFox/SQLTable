fbd65b0b9b399970e0fd35bb8a67e2097ad44951
UndefFox <UndefFox@gmail.com>
2024-06-08 10:41:27 +0300

    Homework4

--- exam/tasks/task_N5.sql
+++ exam/tasks/task_N5.sql
@@ -1,16 +1,22 @@
-// Перепиши вопрос
+// Для создания триггера и триггерной функции, которые проверяют,
+// что имя пассажира не пустое при добавлении записи в базу данных,
+// можно использовать PL/pgSQL в PostgreSQL.
 
-CREATE OR REPLACE PROCEDURE update_delayed_flights(p_flight_date DATE)
+-- Создание триггерной функции
+CREATE OR REPLACE FUNCTION check_passenger_name()
+RETURNS TRIGGER
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    UPDATE flights
-    SET status = 'Delayed'
-    WHERE
-        DATE(scheduled_departure) = p_flight_date
-        AND actual_departure IS NOT NULL
-        AND EXTRACT(EPOCH FROM (actual_departure - scheduled_departure)) > 3600;
-
-    RAISE NOTICE 'Status of delayed flights on % has been updated to Delayed.', p_flight_date;
+    IF NEW.passenger_name IS NULL OR TRIM(NEW.passenger_name) = '' THEN
+        RAISE EXCEPTION 'Passenger name cannot be empty';
+    END IF;
+    RETURN NEW;
 END;
 $$;
+
+-- Создание триггера
+CREATE TRIGGER trigger_check_passenger_name
+BEFORE INSERT ON tickets
+FOR EACH ROW
+EXECUTE FUNCTION check_passenger_name();
